1d27d45a2573ca72c2f37a8c1b3ba3a9a6ff5acb
Nurmuhammad <git config --global user.email>
2026-02-23 10:07:35 +0500

    hop

---
+++ src/components/DestinationCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './destination.css';
+
+function DestinationCard({ name, description, image }) {
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default DestinationCard;
